8a74b8d6c363976ddd7e7c2c0bc62e125c34c634
hseindirani <dirani.hussein93@gmail.com>
2025-03-12 00:00:34 +0100

    organization and role controllers

--- security-service/security-service/src/main/java/com/dapm/security_service/controllers/ClientApi/RoleController.java
+++ security-service/security-service/src/main/java/com/dapm/security_service/controllers/ClientApi/RoleController.java
@@ -1,13 +1,19 @@
 package com.dapm.security_service.controllers.ClientApi;
 
+import com.dapm.security_service.models.Organization;
+import com.dapm.security_service.models.Permission;
 import com.dapm.security_service.models.Role;
 import com.dapm.security_service.models.dtos.RoleDto;
+import com.dapm.security_service.repositories.OrganizationRepository;
+import com.dapm.security_service.repositories.PermissionRepository;
 import com.dapm.security_service.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/roles")
@@ -15,6 +21,10 @@ public class RoleController {
 
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private OrganizationRepository organizationRepository;
+    @Autowired
+    private PermissionRepository permissionRepository;
 
     @GetMapping
     public List<RoleDto> getAllRoles() {
@@ -32,11 +42,35 @@ public class RoleController {
     }
 
     @PostMapping
-    public Role createRole(@RequestBody Role role) {
-        if (role.getId() == null) {
-            role.setId(UUID.randomUUID());
+    public Role createRole(@RequestBody RoleDto roleDto) {
+        if (roleDto.getId() == null) {
+            roleDto.setId(UUID.randomUUID());
         }
-        return roleRepository.save(role);
+        Role r=new Role();
+        r.setId(roleDto.getId());
+        r.setName(roleDto.getName());
+
+        Organization organization = organizationRepository.findById(roleDto.getOrganization().getId())
+                .orElseThrow(() -> new IllegalArgumentException("Organization not found"));
+        r.setOrganization(organization);
+
+        //here we are changing set of string permissions to set od permissions
+
+        Set<Permission> permissions = roleDto.getPermissions().stream()
+                .map(permissionName -> {
+                    Permission permission = permissionRepository.findByName(permissionName);
+                    if (permission == null) { // If not found, create a new one
+                        permission = new Permission();
+                        permission.setName(permissionName);
+                        permission = permissionRepository.save(permission); // Save new permission
+                    }
+                    return permission;
+                })
+                .collect(Collectors.toSet());
+
+        r.setPermissions(permissions);
+
+        return roleRepository.save(r);
     }
 
     @PutMapping("/{id}")
--- security-service/security-service/src/main/java/com/dapm/security_service/models/dtos/RoleDto.java
+++ security-service/security-service/src/main/java/com/dapm/security_service/models/dtos/RoleDto.java
@@ -4,21 +4,23 @@ import com.dapm.security_service.models.Organization;
 import com.dapm.security_service.models.Permission;
 import com.dapm.security_service.models.Role;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Data
+@NoArgsConstructor
 public class RoleDto {
     private UUID id;
     private String name;
-    private Organization organization;
+    private OrganizationDto organization;
     private Set<String> permissions;
     public RoleDto(Role role) {
         this.id = role.getId();
         this.name = role.getName();
-        this.organization = role.getOrganization();
+        this.organization = new OrganizationDto(role.getOrganization()) ;
         this.permissions = role.getPermissions().stream()
                 .map(Permission::getName)
                 .collect(Collectors.toSet());
