b37890f100202c4b05d119f497937b66ad459bb3
CBecke <christian.becke@outlook.dk>
2025-04-26 09:57:26 +0200

    configs made

--- dapm-pipeline/src/main/java/candidate_validation/Pipeline.java
+++ dapm-pipeline/src/main/java/candidate_validation/Pipeline.java
@@ -9,9 +9,9 @@ public class Pipeline {
     private final Set<ProcessingElementReference> elements;
     private final Set<ChannelReference> channels;
 
-    public Pipeline(PipelineCandidate candidate) {
+    public Pipeline(PipelineCandidate candidate) throws InvalidCandidate {
         if (!PipelineCandidateValidator.isValid(candidate)) {
-            throw new InvalidCandidate("Candidate does not meet requirements");
+            throw new InvalidCandidate("Candidate does not meet requirements for a pipeline, e.g. acyclicity, connectedness, etc.");
         }
         this.elements = candidate.getElements();
         this.channels = candidate.getChannels();
