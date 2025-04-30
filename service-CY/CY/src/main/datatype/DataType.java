3a0abcc5b8a7a5322f4deaed8fd7685975cf68b6
Yonna <yonnaklaassen@gmail.com>
2025-04-30 11:30:09 +0200

    Delete folder

--- service-CY/CY/src/main/datatype/DataType.java
+++
@@ -1,9 +0,0 @@
-package main.datatype;
-
-import main.datatype.visitorpattern.Visitor;
-
-/** The type of items which can go over the stream. */
-public abstract class DataType {
-
-    public abstract void acceptVisitor(Visitor<?> v);
-}
