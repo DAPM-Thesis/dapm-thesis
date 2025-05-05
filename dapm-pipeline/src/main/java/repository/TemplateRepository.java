907e791f944831cf0d5a03e69203b36a38ee4929
Yonna <yonnaklaassen@gmail.com>
2025-05-05 15:21:21 +0200

    Move controller and template repository

---
+++ dapm-pipeline/src/main/java/repository/TemplateRepository.java
@@ -0,0 +1,33 @@
+package repository;
+
+import org.springframework.stereotype.Repository;
+import pipeline.processingelement.ProcessingElement;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class TemplateRepository {
+
+    private Map<String, Class<? extends ProcessingElement>> templates;
+
+    public TemplateRepository() {
+        templates = new HashMap<>();
+    }
+
+    public void storeTemplate(String templateID, Class<? extends ProcessingElement> template) {
+        templates.put(templateID, template);
+    }
+
+    public <T extends ProcessingElement> T createInstanceFromTemplate(String templateID) {
+        Class<? extends ProcessingElement> template = templates.get(templateID);
+        if(template == null) {throw new RuntimeException("No template found for template ID: " + templateID); }
+            try {
+                return (T) template.getDeclaredConstructor().newInstance();
+            } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                     NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+    }
+}
