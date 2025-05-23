9958e6958322cf9e523a069a9c2c553df130ce23
Yonna <yonnaklaassen@gmail.com>
2025-05-23 15:47:28 +0200

    Don't allow duplicate templateIDs

--- dapm-pipeline/src/main/java/repository/TemplateRepository.java
+++ dapm-pipeline/src/main/java/repository/TemplateRepository.java
@@ -14,18 +14,19 @@ public class TemplateRepository {
     private final Map<String, Class<? extends ProcessingElement>> templates = new HashMap<>();
 
     public void storeTemplate(String templateID, Class<? extends ProcessingElement> template) {
+        if (templates.containsKey(templateID)) { throw new IllegalArgumentException("Template ID already exists: " + templateID); }
         templates.put(templateID, template);
     }
 
     public <T extends ProcessingElement> T createInstanceFromTemplate(String templateID, Configuration configuration) {
         Class<? extends ProcessingElement> template = templates.get(templateID);
-        if(template == null) {throw new RuntimeException("No template found for template ID: " + templateID); }
-            try {
-                return (T) template.getDeclaredConstructor(Configuration.class).newInstance(configuration);
-            } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
-                     NoSuchMethodException e) {
-                throw new RuntimeException(e);
-            }
+        if (template == null) { throw new RuntimeException("No template found for template ID: " + templateID); }
+        try {
+            return (T) template.getDeclaredConstructor(Configuration.class).newInstance(configuration);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
